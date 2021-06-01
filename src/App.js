d2975712db80e01ceab0b2558cec62cab6740cd0
JoelSilvi <analistasilvi@gmail.com>
2021-06-01 18:49:23 -0400

    básicos React

--- src/App.js
+++ src/App.js
@@ -1,22 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import Componente from "./components/Componente";
+import Propiedades from "./components/Propiedades";
+import Estado from "./components/Estado";
+import RenderizadoCondicional from "./components/RenderizadoCondicional";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <section>
+          <img src={logo} className="App-logo" alt="logo" />
+
+          <p>
+            Editar <code>src/App.js</code> and save to reload.
+          </p>
+
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </section>
+        <section>
+          <Componente msg="Hola soy un Componente funcional expresado desde una prop" />
+          <hr />
+          <Propiedades
+            cadena="cadena de texto"
+            numero={19}
+            booleano={true}
+            arreglo={[1, 2, 3]}
+            objeto={{ nombre: "joel", correo: "joel@gmail.com" }}
+            funcion={(num) => num * num}
+            elementoReact={<i>Esto es un elemento React</i>}
+            componenteReact={
+              <Componente msg="Soy un Componente pasado como Prop" />
+            }
+          />
+          <hr />
+          <Estado />
+          <hr />
+          <RenderizadoCondicional />
+        </section>
       </header>
     </div>
   );
