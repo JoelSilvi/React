7e0c20f7a76f639f8a465b28375f9b92422eb4fe
JoelSilvi <analistasilvi@gmail.com>
2021-06-13 22:59:46 -0400

    Peticiones HTTP a la API ( 5 / 5 )

--- src/components/CrudApi.js
+++ src/components/CrudApi.js
@@ -19,7 +19,7 @@ const CrudApi = () => {
     helpHttp()
       .get(url)
       .then((res) => {
-        console.log(res);
+        // console.log(res);
         if (!res.err) {
           setDb(res);
           setError(null);
@@ -34,20 +34,59 @@ const CrudApi = () => {
 
   const createData = (data) => {
     data.id = Date.now();
+
+    let options = {
+      body: data,
+      headers: { "content-type": "application/json" },
+    };
+    api.post(url, options).then((res) => {
+      console.log(res);
+      if (!res.err) {
+        setDb([...db, res]);
+      } else {
+        setError(res);
+      }
+    });
     setDb([...db, data]);
   };
   const updateData = (data) => {
-    let newData = db.map((el) => (el.id === data.id ? data : el));
-    setDb(newData);
+    let endpoint = `${url}/${data.id} `;
+    // console.log(endpoint);
+
+    let options = {
+      body: data,
+      headers: { "content-type": "application/json" },
+    };
+    api.put(endpoint, options).then((res) => {
+      // console.log(res);
+      if (!res.err) {
+        let newData = db.map((el) => (el.id === data.id ? data : el));
+        setDb(newData);
+      } else {
+        setError(res);
+      }
+    });
   };
+
   const deleteData = (id) => {
     let isDelete = window.confirm(
       `¿Estás seguro de eliminar el registron el id '${id}'?`
     );
 
     if (isDelete) {
-      let newData = db.filter((el) => el.id !== id);
-      setDb(newData);
+      let endpoint = `${url}/${id}`;
+      let options = {
+        headers: { "content-type": "applications/json" },
+      };
+
+      api.del(endpoint, options).then((res) => {
+        if (!res.err) {
+          let newData = db.filter((el) => el.id !== id);
+          setDb(newData);
+        } else {
+          setError(res);
+        }
+      });
     } else {
       return;
     }
