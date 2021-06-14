9a6aec9446efca0b987f37af6266ea3c61a8b328
JoelSilvi <analistasilvi@gmail.com>
2021-06-14 02:27:49 -0400

    Peticiones AJAX a las APIs ( 3 / 5 )

--- src/components/SongSearch.js
+++ src/components/SongSearch.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { helpHttp } from "../helpers/helpHttp";
 import Loader from "./Loader";
 import SongForm from "./SongForm";
 import SongDetails from "./SongDetails";
@@ -9,8 +10,36 @@ const SongSearch = () => {
   const [bio, SetBio] = useState(null);
   const [loading, setLoading] = useState(false);
 
+  useEffect(() => {
+    if (search === null) return;
+
+    const fetchData = async () => {
+      const { artist, song } = search;
+
+      let artistUrl = `https://www.theaudiodb.com/api/v1/json/1/search.php?s=${artist}`;
+      let songUrl = `https://api.lyrics.ovh/v1/${artist}/${song}`;
+
+      console.log(artistUrl, songUrl);
+
+      setLoading(true);
+
+      const [artistRes, songRes] = await Promise.all([
+        helpHttp().get(artistUrl),
+        helpHttp().get(songUrl),
+      ]);
+
+      console.log(artistRes, songRes);
+
+      SetBio(artistRes);
+      setLyric(songRes);
+      setLoading(false);
+    };
+
+    fetchData();
+  }, [search]);
+
   const handleSearch = (data) => {
-    // console.log(data);
+    console.log(data);
     setSearch(data);
   };
 
