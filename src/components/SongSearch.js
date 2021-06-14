c1e78e0e8882e1ae1e0fe4c3d70da5624f01e8f5
JoelSilvi <analistasilvi@gmail.com>
2021-06-14 00:35:06 -0400

    Definición de componentes y lógica ( 1 / 5 )

---
+++ src/components/SongSearch.js
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from "react";
+import Loader from "./Loader";
+import SongForm from "./SongForm";
+import SongDetails from "./SongDetails";
+
+const SongSearch = () => {
+  const [search, setSearch] = useState(null);
+  const [lyric, setLyric] = useState(null);
+  const [bio, SetBio] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const handleSearch = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <div>
+      <h2>Song Search</h2>
+      {loading && <Loader />}
+      <SongForm handleSearch={handleSearch} />
+      <SongDetails search={search} lyric={lyric} bio={bio} />
+    </div>
+  );
+};
+
+export default SongSearch;
