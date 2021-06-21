2fcd54a777dc8f66ac5c934d3ddd42e8d35d80a7
JoelSilvi <analistasilvi@gmail.com>
2021-06-21 14:49:58 -0400

    Funcionalidad y reutilización del componente ( 3 / 3 )

--- src/components/Modals.js
+++ src/components/Modals.js
@@ -1,17 +1,25 @@
+import { useModal } from "../hooks/useModal";
 import Modal from "./Modal";
+import ContactForm from "./ContactForm";
+import SongSearch from "./SongSearch";
 
 const Modals = () => {
+  const [isOpenModal1, openModal1, closeModal1] = useModal(false);
+  const [isOpenModal2, openModal2, closeModal2] = useModal(false);
+  const [isOpenContact, openModalContact, closeModalContact] = useModal(false);
+  const [isOpenSong, openModalSong, closeModalSong] = useModal(false);
+
   return (
     <div>
       <h2>Modales</h2>
-      <button>Modal 1</button>
-      <Modal>
+      <button onClick={openModal1}>Modal 1</button>
+      <Modal isOpen={isOpenModal1} closeModal={closeModal1}>
         <h3>Modal 1</h3>
         <p>Hola este es el contenido de mi modal 1</p>
         <img src="https://placeimg.com/400/400/animals" alt="Animals" />
       </Modal>
-      <button>Modal 2</button>
-      <Modal>
+      <button onClick={openModal2}>Modal 2</button>
+      <Modal isOpen={isOpenModal2} closeModal={closeModal2}>
         <h3>Otro Modal</h3>
         <p>
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aperiam,
@@ -20,6 +28,15 @@ const Modals = () => {
           tempora voluptatem modi.
         </p>
         <img src="https://placeimg.com/400/400/nature" alt="Nature" />
+      </Modal>
+      <button onClick={openModalContact}>Modal Contacto</button>
+      <Modal isOpen={isOpenContact} closeModal={closeModalContact}>
+        <ContactForm />
+      </Modal>
+
+      <button onClick={openModalSong}>Modal de Canciones</button>
+      <Modal isOpen={isOpenSong} closeModal={closeModalSong}>
+        <SongSearch />
       </Modal>
     </div>
   );
---
+++ src/hooks/useModal.js
@@ -0,0 +1,10 @@
+import { useState } from "react";
+
+export const useModal = (initalValue = false) => {
+  const [isOpen, setIsOpen] = useState(initalValue);
+
+  const openModal = () => setIsOpen(true);
+  const closeModal = () => setIsOpen(false);
+
+  return [isOpen, openModal, closeModal];
+};
