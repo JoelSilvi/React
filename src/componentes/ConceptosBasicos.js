c328ee65aeae0c47177600d2ce9fbf4218d48afb
JoelSilvi <analistasilvi@gmail.com>
2021-06-21 23:52:08 -0400

    Sintaxis para declarar rutas

--- src/componentes/ConceptosBasicos.js
+++ src/componentes/ConceptosBasicos.js
@@ -1,4 +1,6 @@
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom"
+import Acerca from "../pages/Acerca";
+import Contacto from "../pages/Contacto"
 const ConceptosBasicos = () =>{
   return(
     <div>
@@ -10,17 +12,12 @@ const ConceptosBasicos = () =>{
         <p>Bienvenid@s al tema de las Rutas en React</p>
       </Route>
       <Route exact path="/acerca">
-        <h3>Acerca</h3>
+        <Acerca/>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem iusto recusandae quidem aliquam ipsum vel culpa veniam atque impedit dolor perspiciatis ullam voluptas, deserunt reiciendis vero? Debitis enim a cupiditate.</p>
       </Route>
-      <Route exact path="/contacto">
-        <h3>Contacto</h3>
-      </Route>
-
-
-
+      {/* <Route exact path="/contacto" component={Contacto}/> */}
+      <Route exact path="/contacto" children={<><Contacto /><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam laboriosam porro explicabo tempore numquam delectus laudantium quam sapiente suscipit omnis? Dolor aliquam recusandae, corrupti repudiandae provident voluptate quidem. Inventore, labore!</p></>}/>
       </Switch>
-
-
     </Router>
     </div>
   )
