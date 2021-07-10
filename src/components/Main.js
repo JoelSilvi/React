3e7d7dbe3aab728078d9baa6cf14a606a7c1a9e0
JoelSilvi <analistasilvi@gmail.com>
2021-07-10 16:54:19 -0400

    Haciendo una APP con THEME Dark/Light SIN Context

---
+++ src/components/Main.js
@@ -0,0 +1,11 @@
+const Main = ({ theme }) => {
+  return (
+    <main className={theme}>
+      <p>Bienvenido Invitado</p>
+      <p>Hola Usuario</p>
+      <p>Mi contenido principal</p>
+    </main>
+  );
+};
+
+export default Main;
